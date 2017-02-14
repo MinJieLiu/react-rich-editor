0595f35af59a7410356354becf1c274a94a3f0e7
MinJieLiu <liumingyi_1@163.com>
2017-02-14 10:13:42 +0800

    bundle .

--- example/index.js
+++ example/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import draftToHtml from 'draftjs-to-html';
-// import draftToMarkdown from 'draftjs-to-markdown';
 import {
   convertFromHTML,
   convertToRaw,
